8029894a40da5938943556adb828f3aa3c27a67e
Jim Biesheuvel <jimbiesheuvel@gmail.com>
2025-04-01 22:33:19 +0200

    programma's gecombineerd

---
+++ gecombineerd.py
@@ -0,0 +1,29 @@
+#PROGRAMMA 1:
+#Er wordt vanuit gegaan dat er integers worden ingevoerd
+
+a = int(input("Eerste getal is: "))
+b = int(input("Tweede getal is: "))
+
+print(a==b)
+
+
+#PROGRAMMA 2:
+#Er wordt vanuit gegaan dat er tussen 2 integers 1 komma wordt ingevoerd
+
+c = input("Geef 2 getallen gescheiden door een komma: ")
+
+c_lijst = c.split(",")
+
+d = c_lijst[0]
+e = c_lijst[1]
+
+print(d==e)
+
+
+#EXCEL SHEET:
+#Ik heb online gekeken hoe ik excel sheets importeer naar Python
+
+import pandas as pd
+
+data = pd.read_excel("IsGelijk.xlsx")
+print(data)
